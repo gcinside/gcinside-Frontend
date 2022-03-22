5122da5ed90285e08f0dee5df27f39652a27b991
Nayejun <ohman0705@naver.com>
2022-03-23 00:12:44 +0900

    [ADD] _document.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+      <>
+        <Head>
+          <title>setting</title>
+          <meta name="description" content="setting"/>
+          <link rel="icon" href="#"/>
+        </Head>
+        <div>hello</div>
+      </>
+  )
+}
+
+export default Home
