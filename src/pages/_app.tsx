d4c7fab3961e1e16555a7aee060f7c7232ef43bf
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2022-03-30 17:31:29 +0900

    Add 로그인 화면 사이드바 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,17 @@ import { Global } from '@emotion/react'
 import SideBar from '../components/Sidebar/Sidebar'
 import GlobalStyle from '../Styles/GlobalStyle'
 import { css } from "@emotion/react";
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <div css={css`
       display: flex; 
       background-color: #F9F9F9; 
       `}>
       <Global styles={GlobalStyle}/>
-      <SideBar />
+      {router.pathname === '/' ? null : <SideBar />}
       <Component {...pageProps}/>
     </div>
   )
