8ad96be08d1e56dc50abd6695d79eab29ba7f327
KimGiHong <68697807+KimGiHong@users.noreply.github.com>
2022-03-31 15:26:53 +0900

    Add 나머지는 해주세요

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -3,3 +3,5 @@ export { default as Home } from "./Home";
 export { default as Top20 } from "./Top20";
 export { default as Setting } from "./Setting";
 export { default as ProfileImg } from "./ProfileImg";
+export { default as GithubIcon } from "./Github";
+export { default as GoogleIcon } from "./Google";
